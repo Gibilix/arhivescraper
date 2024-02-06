9725b8da7f793cddad0aa1a8b049211669c81793
Gibilix <64995737+Gibilix@users.noreply.github.com>
2024-02-05 21:54:01 -0800

    Update scraper.py
    Changed functionality so that the programs scrapes metadata from webpage instead (not the story) and creates .csv file for data.

--- scraper.py
+++ scraper.py
@@ -2,13 +2,46 @@
 
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-url = "https://archiveofourown.org/works/38136640/chapters/95270971"
 
+url = "https://archiveofourown.org/works/43870134?view_adult=true"
 re = requests.get(url)
 soup = BeautifulSoup(re.content, "html.parser")
-results = soup.find("div", role="article")
-#print(results.prettify())
-elements = results.find_all("p")
-for element in results:
-	print(element.text)
+
+################################################################
+
+
+tags = {"rating": [], "warning" : [], "category" : [], "fandom" : [], "relationship" : [], "character" : [], "freeform" : []}
+info = {"published" : "", "words" : "", "chapters" : "", "kudos" : "", "bookmarks" : "", "hits": ""}
+
+#scrape tags from metadata
+for tag in tags:
+	meta = soup.find("dd", class_ = tag + " tags")
+	elements = meta.find_all("li")
+	for element in elements:
+		tags[tag].append(element.text)
+
+#scrape story data such as word count
+for data in info:
+	meta = soup.find("dd", class_ = "stats")
+	elements = meta.find_all("dd")
+	for element in elements:
+		info[data] = element.text
+
+filename = 'test.csv'
+
+header = ["Rating", "Archive Warnings", "Categories", "Fandom", "Relationships", "Characters", "Additional Tags"]
+rows = [[], [], [], [], [], [], []]
+
+for i in range(len(rows)):
+	for tag in tags:
+		try:
+			rows[i].append(tags[tag][i])
+		except IndexError:
+			rows[i].append("Empty")
+
+with open(filename, 'w', newline="") as file:
+    csvwriter = csv.writer(file)
+    csvwriter.writerow(header)
+    csvwriter.writerows(rows)
