bbcde78bb1c59126d541240714f91b02c210e085
Gibilix <64995737+Gibilix@users.noreply.github.com>
2024-02-06 07:22:21 -0800

    Update scraper.py
    fixed problems and added stats

--- scraper.py
+++ scraper.py
@@ -23,23 +23,33 @@ for tag in tags:
 		tags[tag].append(element.text)
 
 #scrape story data such as word count
+meta = soup.find("dd", class_ = "stats")
+elements = meta.find_all("dd")
+n = 0
 for data in info:
-	meta = soup.find("dd", class_ = "stats")
-	elements = meta.find_all("dd")
-	for element in elements:
-		info[data] = element.text
+	info[data] = elements[n].text
+	n += 1
 
 filename = 'test.csv'
 
-header = ["Rating", "Archive Warnings", "Categories", "Fandom", "Relationships", "Characters", "Additional Tags"]
-rows = [[], [], [], [], [], [], []]
+header = ["Rating", "Archive Warnings", "Categories", "Fandom", "Relationships", "Characters", "Additional Tags", \
+		  "Published", "Words", "Chapters", "Kudos", "Bookmarks", "Hits"]
 
-for i in range(len(rows)):
+lst = [len(tags[tag]) for tag in tags]
+mx = max(lst)
+
+rows = [[] for x in range(mx)]
+
+for i in range(mx):
 	for tag in tags:
 		try:
 			rows[i].append(tags[tag][i])
 		except IndexError:
 			rows[i].append("Empty")
+
+for data in info:
+	rows[0].append(info[data])
+
 
 with open(filename, 'w', newline="") as file:
     csvwriter = csv.writer(file)
