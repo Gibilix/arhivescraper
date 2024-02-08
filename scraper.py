bf12f50690d5cc55b1bb6a0cbb82956fd4498bb2
Gibilix <64995737+Gibilix@users.noreply.github.com>
2024-02-08 00:20:58 -0800

    Update scraper.py
    final cut (I hope)

--- scraper.py
+++ scraper.py
@@ -5,53 +5,93 @@ from bs4 import BeautifulSoup
 import csv
 
 
-url = "https://archiveofourown.org/works/43870134?view_adult=true"
+url = "https://archiveofourown.org/works/21483295/chapters/51200008"
 re = requests.get(url)
 soup = BeautifulSoup(re.content, "html.parser")
 
-################################################################
+tags = {"category": [], "genre": [], "language": "", "status" : "", "published" : "", "updated" : "", \
+"Packaged" : "", "rating" : [], "chapters" : "", "words" : "", "publisher" : "www.archiveofourown.org", \
+"story url" : url, "author url" : "", "summary" : [], "fullstory" : []}
 
+genres = ["action", "adventure", "comedy", "crime and mystery", "death game", "fantasy", "historical", "horror", \
+"romance", "satire", "science fiction", "cyberpunk and derivatives", "speculative", "thriller", "isekai", "western"]
 
-tags = {"rating": [], "warning" : [], "category" : [], "fandom" : [], "relationship" : [], "character" : [], "freeform" : []}
-info = {"published" : "", "words" : "", "chapters" : "", "kudos" : "", "bookmarks" : "", "hits": ""}
-
-#scrape tags from metadata
+#technical stuff
 for tag in tags:
-	meta = soup.find("dd", class_ = tag + " tags")
-	elements = meta.find_all("li")
-	for element in elements:
-		tags[tag].append(element.text)
+	if tag == "language":
+		tags[tag] = soup.find("dd", class_ = tag).text
+	elif tag == "rating":
+		text = soup.find("dd", class_ = tag + " tags").text
+		tags[tag] = text
+	elif tag == "fullstory":
+		results = soup.find("div", role="article")
+		#print(results.prettify())
+		elements = results.find_all("p")
+		for element in results:
+			tags[tag].append(element.text)
+	elif tag == "summary":
+		results = soup.find("div", class_="summary module")
+		elements = results.find_all("p")
+		for element in elements:
+			tags[tag].append(element.text)
+	elif tag == "author url":
+		results = soup.find("a", rel="author").text
+		tags[tag] = "https://archiveofourown.org/users/" + results
+	elif tag == "status":
+		results = soup.find("dd", class_ = "stats")
+		elements = results.find_all("dt", class_ = tag)
+		for element in elements:
+			tags[tag] = element.text
+	elif tag == "updated":
+		results = soup.find("dd", class_ = "stats")
+		elements = results.find_all("dd", class_ = "status")
+		for element in elements:
+			tags[tag] = element.text 
+	elif tag == "category":
+		results = soup.find("dd", class_ = "fandom tags")
+		elements = results.find_all("li")
+		for element in elements:
+			tags[tag].append(element.text)
+	elif tag == "genre":
+		results = soup.find("dd", class_ = "freeform tags")
+		elements = results.find_all("li")
+		for element in elements:
+			if element.text.lower() in genres:
+				tags[tag].append(element.text)
+	elif tag == "package":
+		continue;
+	else:
+		meta = soup.find("dd", class_ = "stats")
+		elements = meta.find_all("dd", class_ = tag)
+		for element in elements:
+			tags[tag] = element.text
+#title of fanfic
+title = soup.find("h2", class_ = "title heading").text
+#header (top row) for csv
+header = ["Category", "Genre", "Language", "Status", "Published", "Updated", \
+"Packaged", "Rating", "Chapters", "Words", "Publisher", "Story URL", "Author URL", "Summary", "FullStory"]
+#first and only row for csv
+row = ["" for i in range(len(header))]
 
-#scrape story data such as word count
-meta = soup.find("dd", class_ = "stats")
-elements = meta.find_all("dd")
-n = 0
-for data in info:
-	info[data] = elements[n].text
-	n += 1
-
-filename = 'test.csv'
-
-header = ["Rating", "Archive Warnings", "Categories", "Fandom", "Relationships", "Characters", "Additional Tags", \
-		  "Published", "Words", "Chapters", "Kudos", "Bookmarks", "Hits"]
-
-lst = [len(tags[tag]) for tag in tags]
-mx = max(lst)
-
-rows = [[] for x in range(mx)]
-
-for i in range(mx):
+#creating the row
+for i in range(len(header)):
 	for tag in tags:
-		try:
-			rows[i].append(tags[tag][i])
-		except IndexError:
-			rows[i].append("Empty")
+		if header[i].lower() == tag:
+			if type(tags[tag]) is list:
+				row[i] = " ".join(tags[tag])
+			else:
+				row[i] = tags[tag]
 
-for data in info:
-	rows[0].append(info[data])
+#cleaning up the row elements
+for r in range(len(row)):
+	row[r] = row[r].strip()
 
+row[3] = row[3].replace(":", "")
+row[8] = row[8].split("/")[-1]
+#print(row)
 
+filename = ",".join(tags["category"]) + " - " + tags["author url"].split("/", 4)[4] + " - " + title.strip() + ".csv"
 with open(filename, 'w', newline="") as file:
     csvwriter = csv.writer(file)
     csvwriter.writerow(header)
-    csvwriter.writerows(rows)
+    csvwriter.writerow(row)
